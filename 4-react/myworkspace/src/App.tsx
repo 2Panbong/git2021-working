fb4fc417cf02c28d776d1c1f86dc13b3105cdd27
LEE HAK BONG <flamex2@naver.com>
2021-08-21 20:36:07 +0900

    0820 복습

--- 4-react/myworkspace/src/App.tsx
+++ 4-react/myworkspace/src/App.tsx
@@ -1,20 +1,20 @@
-// JSX: Javascript 기반의 HTML 태그 형식
+// JSX : Javscript 기반의 HTML 태그 형식
 // 각각의 태그(element)들은 javascript 객체임
 // 일반적인 html 태그 표기법과 다름
 
 // JSX Element
 // const element = (
-//  <h1 className="greeting">
-//    Hello, world!
-//    <h1>
+//    <h1 className = "greeting">
+//      Hello, world!
+//    </h1>
 //  );
 
-//실제 컴파일되는 결과
+// 실제 컴파일되는 결과
 // const element = React.createElement(
-//   'h1',// 태그 종류
-// {className: 'greeting'}, // 속성
-// 'Hello,wordl!'//컨텐츠
-// );
+//   'h1', // 태그 종류
+//   {className: 'greeting'}, // 속성
+//   'Hello, world!'  // 컨텐트
+// )
 
 // document.createElement("div")
 // 실제 DOM을 생성함
@@ -25,96 +25,81 @@
 // 내부적으로 가상 DOM tree를 관리함
 
 // 렌더링(rendering) : 화면에 그리기
-// 가상 DOM을 생성하고 렌더링 시점(event loop)에 가상DOM을 HTML DOM으로 그림
+// 가상DOM을 생성하고 렌더링시점(event loop)에 가상DOM을 HTML DOM으로 그림
 
 // 일반 DOM
-// DOM을 조작할 때마다 rendering함, 성능 저하
+// DOM을 조작할 때마다 rendering 함, 성능 저하
 
 // 가상 DOM
-// 렌더링 주기에 따라서 변동사항만 렌더링함, 성능 향상
+// 렌더링ㄹ 주기에 따라서 변동사항만 렌더링함
 
-// recat 관련 자료는 2020년 이후 것으로만
+// react 관련 자료는 2020년 이후 것으로만
+
 // Function Component
-// 리액트는 함수이름이 대문자로 시작하는게 있음
+// 대문자로 시작함
 // JSX Element를 반환함
-// JS함수인데, JSX Element를 반환함 == Component
+// JS함수인데 , JSX Element를 반환함 == Component
 // 리액트에서 컴포넌트는 JSX Element를 렌더링하는 함수
 
+// https://react.vlpt.us/styling/02-css-module.html
+// css module
+// 파일명.module.css
+// css를 사용하는 컴포넌트 범위로 css class 사용범위를 좁힐 수 있음
+
 import "./App.scss";
-import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom"
-import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import Header from "./components/Header";
-import Button from "./components/Button";
-import Counter from "./components/Counter";
-import Calculator from "./components/Calculator";
-import Generator from "./components/Generator";
-import Hello from "./components/Hello";
-import Components from './components/Components';
-import AccountManager from './components/AccountManager';
+import Home from "./components/Home";
+import Navigation from "./Navigation";
 
+// SPA(Single Page Application)
+// :페이지 파일이 1개, index.htmp
+// : 특정 영역에 컴포넌트(js)를 로딩함
+// : 에플리케이션 처음 로딩될 때 import한  컴포넌트를 로딩함
+// 	-> 컴파일 됐을 때 파일크기가 커짐, 초기 로딩할 때 시간 걸림
 
+const Counter = lazy(() => import("./components/Counter"));
+const Calculator = lazy(() => import("./components/CalculatorRef"));
+const Generator = lazy(() => import("./components/Generator"));
+const AccountManager = lazy(() => import("./components/AccountManagerRef"));
+const Components = lazy(() => import("./components/Components"));
+const BootStrap = lazy(() => import("./components/Bootstrap"));
 
 // React == 컴포넌트 개발 라이브러리
 function App() {
   return (
     <Router>
-    // main-container
-    <div style={{ width: "500px", margin: "0 auto" }}>
-      <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="counter">Counter</Link></li>
-      <li><Link to="calculator">Calculator</Link></li>
-      <li><Link to="generator">Generator</Link></li>
-      <li><Link to="account">AccountManager</Link></li>
-      <li><Link to="account">BootStrap</Link></li>
-    </ul>
-      </nav>
-      <main>
-      <Switch>
-        {/* Suspense 컴포넌트 로딩중에 보여줄 화면을 처리하는 것 */}
-        {/* fallback={로딩중에 보여줄 컴포넌트} */}
-        <Suspense fallback={<div>Loading...</div>}
-        {/* Swtich 영역에 컴포넌트가 로딩됨 */}
+      {/* main container */}
+      <div style={{ width: "900px" }} className="mx-auto">
+        <nav
+          style={{ width: "200px", height: "100vh", top: "20px" }}
+          className="position-fixed"
+        >
+          <Navigation />
+        </nav>
+        <main style={{ marginLeft: "200px", marginTop: "20px" }}>
+          {/* Suspense 컴포넌트로 로딩중에 보여줄 화면을 처리하는 것 */}
+          {/* fallback ={로딩중에 보여줄 컴포넌트} */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              {/* Switch 영역에 컴포넌트가 로딩됨 */}
 
-        {/* 해당 경로에 대해서 로딩할 컴포넌트 목록을 작성 */}
-        <Route path="/" component= {Components} exact />
-        <Route path="/counter" component ={Counter} />
-        <Route path= "/calculator" component={Calculator} />
-        <Route path= "/generator" component={Generator} />
-        <Route path= "account=manager" component= {AccountManager}/>
-        <Route path = "/bootstrap" component= {BootStrap}/>
-        
-      </Switch>
-      </main>
-      <button className="btb btn-primary">Primary</button>
-      <button className="btb btn-danger">danger</button>
-      {/* JSX 내부에서 주석 달기 */}
-      {/* 재사용하지 않는 컴포넌트 */}
-      <h1 style={{ color: "red" }}>Hello React with Typescript !</h1>
-
-      {/* 속성값을 변경하여 재사용하는 컴포넌트 */}
-      {/* Component의 속성(prop)을 넘김 */}
-      {/* 속성명 = {속성값} */}
-      <Header color={"red"} title={"React"} />
-      <Header color={"green"} title={"Typescript"} />
-      <Header color={"blue"} title={"Function Component"} />
-
-      <Button color={"White"} backgroundColor={"blue"} text={"Add"} />
-      <Button color={"black"} backgroundColor={"red"} text={"Delete"} />
-      <Button color={"black"} backgroundColor={"green"} text={"Done"} />
-      {/* <Header color={"blue"} title={"Typescript"}/>  */}
-      {/* <Header color={"red"} title= {"React"}/> */}
-
-      <Counter />
-      <Calculator />
-      <Generator />
-      <Hello />
-    </div>
+              {/* 해당 경로에 대해서 로딩할 컴포넌트 목록을 작성 */}
+              <Route path="/" component={Home} exact />
+              <Route path="/components" component={Components} />
+              <Route path="/counter" component={Counter} />
+              <Route path="/calculator" component={Calculator} />
+              <Route path="/generator" component={Generator} />
+              <Route path="/account-manager" component={AccountManager} />
+              <Route path="/bootstrap" component={BootStrap} />
+            </Switch>
+          </Suspense>
+        </main>
+      </div>
     </Router>
   );
 }
 
-// App.tsx 모듈의 기본 내보내기를 App 함수로 함
+// APP.tsx 모듈의 기본 내보내기를 App 함수로 함
 export default App;
